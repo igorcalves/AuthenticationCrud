82066deca422bd2f5b9a5a086250cead59f02edc
igor alves <112185065+igorcalves@users.noreply.github.com>
2024-02-10 01:28:59 -0300

    first commit

---
+++ authenticationCrud/src/main/java/br/com/authenticationWithCrud/authenticationCrud/AuthenticationCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.authenticationWithCrud.authenticationCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationCrudApplication.class, args);
+	}
+
+}
---
+++ authenticationCrud/src/test/java/br/com/authenticationWithCrud/authenticationCrud/AuthenticationCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.authenticationWithCrud.authenticationCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
